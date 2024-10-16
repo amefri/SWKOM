9be4262182314182bebd7b0700aa114f8f7273d7
Patricia <tpatricia.stefania@gmail.com>
2024-10-16 12:44:38 +0200

    ApiSetup and adaptations
    Files adapted to meet the requirements

---
+++ out/src/main/java/at/fhtw/swkom/paperless/controller/PaperlessApiController.java
@@ -0,0 +1,27 @@
+package at.fhtw.swkom.paperless.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.context.request.NativeWebRequest;
+
+import java.util.Optional;
+
+@Controller
+@RequestMapping("/api")
+public class PaperlessApiController implements PaperlessApi {
+
+    private final NativeWebRequest request;
+
+    @Autowired
+    public PaperlessApiController(NativeWebRequest request) {
+        this.request = request;
+    }
+
+    @Override
+    public Optional<NativeWebRequest> getRequest() {
+        return Optional.ofNullable(request);
+    }
+
+    // Hier kannst du bei Bedarf die Methoden aus PaperlessApi überschreiben und mit richtiger Logik füllen.
+}
--- out/src/main/java/at/fhtw/swkom/paperless/controller/PaperlessUtil.java
+++ out/src/main/java/at/fhtw/swkom/paperless/controller/PaperlessUtil.java
@@ -5,7 +5,7 @@ import org.springframework.web.context.request.NativeWebRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public class ApiUtil {
+public class PaperlessUtil {
     public static void setExampleResponse(NativeWebRequest req, String contentType, String example) {
         try {
             HttpServletResponse res = req.getNativeResponse(HttpServletResponse.class);
