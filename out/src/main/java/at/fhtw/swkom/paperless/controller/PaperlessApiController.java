512873df0fc3600fffa59b5e1f5ad2a860e60cb7
Patricia <tpatricia.stefania@gmail.com>
2024-10-17 15:56:53 +0200

    Controller first implementation

--- out/src/main/java/at/fhtw/swkom/paperless/controller/PaperlessApiController.java
+++ out/src/main/java/at/fhtw/swkom/paperless/controller/PaperlessApiController.java
@@ -1,10 +1,15 @@
 package at.fhtw.swkom.paperless.controller;
 
+import at.fhtw.swkom.paperless.services.dto.Document;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.util.List;
 import java.util.Optional;
 
 @Controller
@@ -21,5 +26,26 @@ public class PaperlessApiController implements PaperlessApi {
     @Override
     public Optional<NativeWebRequest> getRequest() {
         return Optional.ofNullable(request);
+    }
+
+
+    @Override
+    public ResponseEntity<Void> postDocument(String document, MultipartFile file) {
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @Override
+    public ResponseEntity<Void> deleteDocument(Integer id) {
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @Override
+    public ResponseEntity<Document> getDocument(Integer id) {
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @Override
+    public ResponseEntity<List<Document>> getDocuments() {
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
