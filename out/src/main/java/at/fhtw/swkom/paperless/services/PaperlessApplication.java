a13a0ed6f79ca6dc9ce308b731dc98485d201eaa
Amelie Frimberger <amelie.frimberger@gmail.com>
2024-10-18 15:18:37 +0200

    added checker local added in gitignore

--- out/src/main/java/at/fhtw/swkom/paperless/services/PaperlessApplication.java
+++ out/src/main/java/at/fhtw/swkom/paperless/services/PaperlessApplication.java
@@ -1,5 +1,6 @@
 package at.fhtw.swkom.paperless.services;
 
+
 import org.openapitools.jackson.nullable.JsonNullableModule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
