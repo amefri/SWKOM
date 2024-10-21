aee6c2ce93af1b4159b997367f682773a09e7c74
amefri <157507624+amefri@users.noreply.github.com>
2024-10-21 13:29:32 +0200

    updated Hard coded for new Sprint grading

--- out/src/main/java/at/fhtw/swkom/paperless/controller/PaperlessApiController.java
+++ out/src/main/java/at/fhtw/swkom/paperless/controller/PaperlessApiController.java
@@ -3,26 +3,21 @@ package at.fhtw.swkom.paperless.controller;
 import at.fhtw.swkom.paperless.services.dto.Document;
 
 
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.context.request.NativeWebRequest;
 
-import jakarta.validation.constraints.*;
-import jakarta.validation.Valid;
-
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 import jakarta.annotation.Generated;
 
@@ -43,4 +38,45 @@ public class PaperlessApiController implements PaperlessApi {
         return Optional.ofNullable(request);
     }
 
+    @Override
+    public ResponseEntity<Void> deleteDocument(
+            @Parameter(name = "id", description = "The id of the document", required = true, in = ParameterIn.PATH) @PathVariable("id") Integer id
+    ) {
+        // TODO: Replace hc
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @Override
+    public ResponseEntity<Document> getDocument(
+            @Parameter(name = "id", description = "The id of the document", required = true, in = ParameterIn.PATH) @PathVariable("id") Integer id
+    ) {
+        // TODO: Replace hc
+        Document body = new Document("Test Title");
+        return new ResponseEntity<>(body, HttpStatus.OK);
+    }
+
+    @Override
+    public ResponseEntity<List<Document>> getDocuments() {
+        // TODO: Replace
+        Document doc1 = new Document("Test Title 1");
+        Document doc2 = new Document("Test Title 2");
+        List<Document> body = List.of(doc1, doc2);
+        return new ResponseEntity<>(body, HttpStatus.OK);
+    }
+
+    @Override
+    public ResponseEntity<Void> postDocument(
+            @Parameter(name = "document", description = "") @Valid @RequestParam(value = "document", required = false) String document,
+            @Parameter(name = "file", description = "") @RequestPart(value = "file", required = false) MultipartFile file
+    ) {
+        // TODO: Replace HC
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @Override
+    public ResponseEntity<Void> updateDocument(Integer id) {
+
+        //TODO: Implement
+        return PaperlessApi.super.updateDocument(id);
+    }
 }
